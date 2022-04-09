8c783c0935e0f38675ff25048cb621716b4b0df9
phuongdoan13 <mphuongdoant@gmail.com>
2022-04-10 09:59:05 +1000

    Add DS3_Tree, with Tree and BST

---
+++ DS3_Tree/BSTree.java
@@ -0,0 +1,91 @@
+package DS3_Tree;
+
+public class BSTree extends Tree{
+    public BSTree(int val){
+        super(val);
+        
+    }
+    Node search(int val){
+        // the node with the VAL;
+        // Average case: O(log(n)), Worst case: O(n)
+        return searchHelper(this.node, val);
+    }
+    private Node searchHelper(Node node, int VAL){
+        // recursive method for finding node with val
+        // Average case: O(log(n)), Worst case: O(n)
+        if(node == null)
+            return null;
+        if(node.val == VAL)
+            return node;
+        else if(node.val > VAL)
+            return searchHelper(node.right, VAL);
+        else 
+            return searchHelper(node.left, VAL);
+    }
+
+    
+
+    void add(int val){
+        // add new val
+        // Average case: O(log(n)), Worst case: O(n)
+        addHelper(this.node, val);
+    }
+    
+    private Node addHelper(Node node, int VAL){
+        // recursive method for adding VAL
+        // Average case: O(log(n)), Worst case: O(n)
+        if(node == null)
+            return new Node(VAL);
+        if(node.val == VAL)
+            return node;
+        if(node.val < VAL)
+            node.right = addHelper(node.right, VAL);
+        else if(node.val > VAL)
+            node.left = addHelper(node.left, VAL);
+        return node;
+            
+    }
+
+    void delete(int val){
+        // delete the given value;
+        // Average case: O(log(n)), Worst case: O(n)
+        deleteHelper(this.node, val);
+    }
+
+    private Node deleteHelper(Node node, int VAL){
+        // recursive method for deleting a node with given method;
+        // Average case: O(log(n)), Worst case: O(n)
+        if(node == null)
+            return node;
+        // find the node
+        if(node.val < VAL){
+            node.right = deleteHelper(node.right, VAL); 
+        }else if(node.val > VAL){
+            node.left = deleteHelper(node.left, VAL);
+        }else{
+            // if the node has 0 or 1 child;
+            if(node.left == null)
+                return node.right;
+            else if(node.right == null)
+                return node.left;
+
+            // if the node has 2 children;
+            // copy the inorder successor value (i.e. the next smallest value )
+            node.val = minValue(node.right);
+            // delete the next successor node;
+            node.right = deleteHelper(node.right, node.val);
+        }
+        return node;
+    }
+
+    private int minValue(Node node) {
+        // find the min value of the (sub)tree.
+        // Average case: O(log(n)), Worst case: O(n)
+        int minv = node.val;
+        while (node.left != null) {
+            minv = node.left.val;
+            node = node.left;
+        }
+        return minv;
+    }
+}
---
+++ DS3_Tree/Tree.java
@@ -0,0 +1,111 @@
+package DS3_Tree;
+class Node {
+    int val;
+    Node left;
+    Node right;
+    public Node(int val){
+        this.val = val;
+    }
+    public Node(int val, Node left, Node right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Tree{
+    Node node;
+    public Tree(int val){
+        this.node = new Node(val);
+    }
+    boolean isFullBinaryTree(Node node){
+        // Check if a tree is a full binary tree
+        // O(n)
+        if(node == null) return true;
+        if(node.left == null && node.right == null) 
+            return true;
+        if(node.left != null && node.right != null) 
+            return isFullBinaryTree(node.right) && isFullBinaryTree(node.left);
+        return false;
+    }
+    
+    boolean isCompleteBinaryTree(Node node){
+        // Check if a tree is a complete binary tree
+        // O(n)
+        int numberOfNodes = this.countNumberOfNode(node);
+        return checkCompleteBinaryTree(node, 0, numberOfNodes);
+    }
+    
+    int countNumberOfNode(Node node){
+        // Count the number of nodes in a tree
+        // O(n)
+        if(node == null)
+            return 0;
+        return (1 + countNumberOfNode(node.left) + countNumberOfNode(node.right));
+    }
+    
+    boolean checkCompleteBinaryTree(Node node, int index, int numberOfNodes){
+        // Recursive helper function to check a complete binary tree;
+        // O(n)
+        if(node == null)
+            return true;
+        
+        if(index >= numberOfNodes)
+            return false;
+        
+        return checkCompleteBinaryTree(node.left, 2 * index + 1, numberOfNodes) && checkCompleteBinaryTree(node.right, 2 * index + 2, numberOfNodes);
+    }
+    
+    void inorderTraversal(Node node){
+        if(node == null) return;
+        inorderTraversal(node.left);
+        System.out.print(node.val + " ");
+        inorderTraversal(node.right);
+    }
+
+    void preorderTraversal(Node node){
+        if(node == null) return;
+        System.out.print(node.val + " ");
+        inorderTraversal(node.left);
+        inorderTraversal(node.right);
+    }
+
+    void postorderTraversal(Node node){
+        if(node == null) return;
+        inorderTraversal(node.left);
+        inorderTraversal(node.right);
+        System.out.print(node.val + " ");
+    }
+    public static void main(String[] args) {
+        System.out.println("Normal Tree");
+        Tree tree = new Tree(1);
+        tree.node.left = new Node(2);
+        tree.node.right = new Node(3);
+        tree.node.left.right = new Node(5);
+        tree.node.left.left = new Node(4);
+        tree.node.right.left = new Node(6);
+        System.out.println(tree.countNumberOfNode(tree.node));      
+        System.out.println(tree.isFullBinaryTree(tree.node));
+        System.out.println(tree.isCompleteBinaryTree(tree.node));   
+        tree.inorderTraversal(tree.node);System.out.println();
+        tree.preorderTraversal(tree.node);System.out.println();
+        tree.postorderTraversal(tree.node);System.out.println();
+        
+
+        System.out.println("BST");
+        BSTree bst = new BSTree(4);
+        bst.add(3);
+        bst.add(1);
+        bst.add(8);
+        bst.add(0);
+        bst.add(3);
+        bst.add(100);
+        bst.inorderTraversal(bst.node); System.out.println();
+        bst.delete(3);
+        bst.inorderTraversal(bst.node); System.out.println();
+
+
+
+        
+    }
+}
