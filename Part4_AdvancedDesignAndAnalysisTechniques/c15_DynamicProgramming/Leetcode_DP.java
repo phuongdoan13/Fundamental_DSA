06b52456157b9678b14ab60ae11317249ad6c309
phuongdoan13 <mphuongdoant@gmail.com>
2022-09-07 09:43:55 +1000

    Add houseRobber

--- Part4_AdvancedDesignAndAnalysisTechniques/c15_DynamicProgramming/Leetcode_DP.java
+++ Part4_AdvancedDesignAndAnalysisTechniques/c15_DynamicProgramming/Leetcode_DP.java
@@ -81,6 +81,37 @@ public class Leetcode_DP {
         }
         return memo[memo.length - 1];
     }
+
+    public int houseRobber(int[] nums) {
+        /* 
+            Q198. House Robber
+            You are a professional robber planning to rob houses along a street. 
+            Each house has a certain amount of money stashed, 
+            the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected 
+            and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+            Given an integer array nums representing the amount of money of each house, 
+            return the maximum amount of money you can rob tonight without alerting the police.
+            
+            Example 1
+            Input: nums = [1,2,3,1]
+            Output: 4
+            
+        */
+        // O(n) O(1)
+        int robPrevHouse = nums[0];
+        int notRobPrevHouse = 0;
+        
+        for(int i = 1; i < nums.length; i++){
+            int rob =  nums[i] + notRobPrevHouse;
+            int notRob = Math.max(robPrevHouse,notRobPrevHouse);
+            robPrevHouse = rob;
+            notRobPrevHouse = notRob;
+        }
+        return Math.max(robPrevHouse,notRobPrevHouse);
+    }
     public static void main(String[] args) {
+        Leetcode_DP obj = new Leetcode_DP();
+        System.out.println(obj.houseRobber(new int[]{2,7,9,3,1}));
     }
 }
