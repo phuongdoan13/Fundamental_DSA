aad5931b7f03c8771be9229d1a57aea94f120e5e
phuongdoan13 <mphuongdoant@gmail.com>
2022-12-15 20:24:21 +1100

    Add CSES

---
+++ CSES/Introductory/WeirdAlgorithm.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+
+double EPS = 1e-9;
+int INF = 1000000005;
+long long INFF = 1000000000000000005LL;
+double PI = acos(-1);
+int dirx[8] = {-1, 0, 0, 1, -1, -1, 1, 1};
+int diry[8] = {0, 1, -1, 0, -1, 1, -1, 1};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    LL n;
+    cin >> n;
+    while (n != 1)
+    {
+        std::cout << n << " ";
+        if (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n = n * 3 + 1;
+        }
+    }
+    cout << 1;
+    return 0;
+}
