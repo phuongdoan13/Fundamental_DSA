931078ba6d2808abc6671d5291744c25fb3ec16e
phuongdoan13 <mphuongdoant@gmail.com>
2023-08-03 23:52:46 +1000

    Added

---
+++ Part3_DataStructures/cxx_Tree/BinaryTreeNode.java
@@ -0,0 +1,13 @@
+package Part3_DataStructures.cxx_Tree;
+
+public class BinaryTreeNode{
+  int val;
+  BinaryTreeNode left;
+  BinaryTreeNode right;
+  
+  public BinaryTreeNode(){}
+
+  public BinaryTreeNode(int val){
+    this.val = val;
+  }
+}
